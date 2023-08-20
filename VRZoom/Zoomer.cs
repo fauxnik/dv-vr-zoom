235964e645a342af6161f99dd6b8feb12bcdca6f
Niko Fox <niko@fauxnik.me>
2023-08-19 17:00:29 -0700

    beta 1 (needs clean up / rework!)

--- VRZoom/Zoomer.cs
+++ VRZoom/Zoomer.cs
@@ -1,5 +1,6 @@
-using System.Collections;
 using CameraManager;
+using static CameraManager.CameraType;
+using System.Collections;
 using UnityEngine;
 
 namespace VRZoom;
@@ -38,16 +39,17 @@ class Zoomer : MonoBehaviour
 		int resWidth = Screen.width;
 		int resHeight = Screen.height;
 		renderTexture = new RenderTexture(resWidth, resHeight, 24);
-		camera.targetTexture = renderTexture;
-		if (!Main.settings.showOnPC) { camera.enabled = false; }
+		// camera.targetTexture = renderTexture;
+		camera.enabled = false; //Main.settings.showOnPC;
 
 		quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
 		DestroyImmediate(quad.GetComponent<Collider>());
 		quad.transform.SetParent(camera.gameObject.transform, false);
 		quad.transform.localScale = new Vector3(quad.transform.localScale.y * resWidth / resHeight, quad.transform.localScale.y, quad.transform.localScale.z);
 		quad.layer = viewportLayer;
+		// TODO: can we raise this above the World camera so it always appears in front of objects in the scene?
 		camera.cullingMask &= ~(1 << quad.layer);
-		PlayerManager.PlayerCamera.cullingMask |= 1 << quad.layer;
+		CameraAPI.GetCamera(World).cullingMask |= 1 << quad.layer;
 
 		meshRenderer = quad.GetComponent<MeshRenderer>();
 		meshRenderer.enabled = false;
@@ -55,9 +57,14 @@ class Zoomer : MonoBehaviour
 
 		Settings.OnSettingsChanged += OnSettingsChanged;
 		OnSettingsChanged();
+
+		// Camera.onPreRender += OnPreRender;
+		// Camera.onPostRender += OnPostRender;
+
+		StartCoroutine(UpdateScreen());
 	}
 
-	void OnSettingsChanged()
+	private void OnSettingsChanged()
 	{
 		Main.LogDebug?.Invoke($"Settings changed {{\n\tzoomFactor: {Main.settings.zoomFactor}\n\tzoomInDuration: {Main.settings.zoomInDuration}\n\tzoomOutDuration: {Main.settings.zoomOutDuration}\n\tviewportMeters: {Main.settings.viewportMeters}\n\tshowOnPC: {Main.settings.showOnPC}\n}}");
 
@@ -71,22 +78,46 @@ class Zoomer : MonoBehaviour
 		}
 	}
 
-	void OnPreRender()
+	public void Update()
 	{
-		if (camera == null || renderTexture == null || !(Main.modEntry?.Active ?? false)) { return; }
+		Main.LogDebug?.Invoke($"Update {{\n\tcamera: {(camera == null ? "null" : camera)}\n\trenderTexture: {(renderTexture == null ? "null" : renderTexture)}\n\tActive: {Main.modEntry?.Active ?? false}\n\tshowOnPC: {Main.settings.showOnPC}}}");
+
+		if (camera == null || renderTexture == null || meshRenderer == null || !(Main.modEntry?.Active ?? false)) { return; }
+
+		if (meshRenderer.enabled || Main.settings.showOnPC)
+		{
+			camera.targetTexture = renderTexture;
+			camera.Render();
+			camera.targetTexture = null;
+		}
 	}
 
-	void OnPostRender()
+	IEnumerator UpdateScreen()
 	{
-		Main.LogDebug?.Invoke($"OnPostRender {{\n\tcamera: {(camera == null ? "null" : camera.ToString())}\n\trenderTexture: {(renderTexture == null ? "null" : renderTexture.ToString())}\n\tActive: {Main.modEntry?.Active ?? false}\n\tshowOnPC: {Main.settings.showOnPC}}}");
+		yield return new WaitForEndOfFrame();
 
-		if (camera == null || renderTexture == null || !(Main.modEntry?.Active ?? false)) { return; }
-
-		if (Main.settings.showOnPC)
+		if (Main.settings.showOnPC && renderTexture != null)
 		{
 			Graphics.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), renderTexture);
 		}
 	}
+
+	// public void OnPreRender(Camera renderCamera)
+	// {
+	// 	if (camera == null || renderTexture == null || !(Main.modEntry?.Active ?? false) || renderCamera != CameraAPI.GetCamera(World)) { return; }
+	// }
+
+	// public void OnPostRender(Camera renderCamera)
+	// {
+	// 	Main.LogDebug?.Invoke($"OnPostRender {{\n\trender camera: {renderCamera}\n\tcamera: {(camera == null ? "null" : camera)}\n\trenderTexture: {(renderTexture == null ? "null" : renderTexture)}\n\tActive: {Main.modEntry?.Active ?? false}\n\tshowOnPC: {Main.settings.showOnPC}}}");
+
+	// 	if (camera == null || renderTexture == null || !(Main.modEntry?.Active ?? false) || renderCamera != CameraAPI.GetCamera(Effects)) { return; }
+
+	// 	if (Main.settings.showOnPC)
+	// 	{
+	// 		Graphics.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), renderTexture);
+	// 	}
+	// }
 
 	public void Zoom(bool zoomIn)
 	{
@@ -108,7 +139,7 @@ class Zoomer : MonoBehaviour
 		float zoomDuration = zoomIn ? Main.settings.zoomInDuration : Main.settings.zoomOutDuration;
 		Main.LogDebug?.Invoke($"Zoom coroutine [{iteration:D6}]: {camera.zoomFactor} -> {targetZoomFactor}");
 		yield return new WaitForEndOfFrame();
-		camera.enabled = true;
+		// camera.enabled = true;
 		meshRenderer.enabled = zoomIn;
 		yield return new WaitForEndOfFrame();
 		while (!Mathf.Approximately(camera.zoomFactor, targetZoomFactor))
@@ -118,7 +149,7 @@ class Zoomer : MonoBehaviour
 			yield return new WaitForEndOfFrame();
 		}
 		camera.zoomFactor = targetZoomFactor;
-		if (!zoomIn && !Main.settings.showOnPC) { camera.enabled = false; }
+		// if (!zoomIn && !Main.settings.showOnPC) { camera.enabled = false; }
 		Main.LogDebug?.Invoke($"Zoom coroutine [{iteration:D6}]: complete");
 		zoomCoroutine = null;
 	}
